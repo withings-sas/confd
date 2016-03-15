bb5ead494380003c76f87f097c039c6d58cf12ce
Etienne Adam <etienne.adam@withings.com>
2016-03-15 18:16:20 +0100

    [build] add github.com/kelseyhightower/confd

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 export GOPATH=$1
 
+# golang installed from ppa:ubuntu-lxc/lxd-stable
+
 go get github.com/BurntSushi/toml
 go get github.com/go-sql-driver/mysql
+go get github.com/kelseyhightower/confd
 
 echo "Building confd..."
 mkdir -p bin
