0f84c8270ed86fcd610864068086923ca61cdc0c
Etienne Adam <etienne.adam@withings.com>
2016-03-15 18:14:34 +0100

    [build] give go path as argument

--- predeploy.sh
+++ predeploy.sh
@@ -19,11 +19,11 @@ BUILD_SERVER=fr-hq-build-02.corp.withings.com
 BUILD_USER=scaleweb
 BUILD_PATH=/home/$BUILD_USER/confd-$ENVIRONMENT
 
-ssh $BUILD_USER@$BUILD_SERVER "mkdir -p $BUILD_PATH"
+ssh $BUILD_USER@$BUILD_SERVER "mkdir -p $BUILD_PATH/go"
 
-rsync -az --delete --exclude="bin/" ./ $BUILD_USER@$BUILD_SERVER:$BUILD_PATH/
+rsync -az --delete --exclude="bin/" --exclude="go/" ./ $BUILD_USER@$BUILD_SERVER:$BUILD_PATH/
 
-ssh $BUILD_USER@$BUILD_SERVER "cd $BUILD_PATH; bash build.sh"
+ssh $BUILD_USER@$BUILD_SERVER "cd $BUILD_PATH; bash build.sh $BUILD_PATH/go"
 
 rsync -avz --dry-run $BUILD_USER@$BUILD_SERVER:$BUILD_PATH/bin/ bin/
 
